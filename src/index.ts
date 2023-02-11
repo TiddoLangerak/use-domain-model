a9657c414f72889c54be1270bb229fe20df9f676
Tiddo Langerak <tiddolangerak@gmail.com>
2023-02-11 14:23:53 +0000

    Support nested reassignment

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
-export function watch<T extends object>(obj: T, onChange: () => void) {
+
+type Watchable = object;
+
+function isWatchable(t: any): t is Watchable {
+    return typeof t === 'object' && !!t;
+}
+
+export function watch<T extends Watchable>(obj: T, onChange: () => void) {
     for (const prop of Object.getOwnPropertyNames(obj)) {
         const original = Object.getOwnPropertyDescriptor(obj, prop);
 
-        let val = obj[prop];
+        let val = (obj as Record<string, unknown>)[prop];
 
-        if (typeof val === 'object' && !!val) {
+        if (isWatchable(val)) {
             watch(val, onChange);
         }
-
-        // TODO: test for after reassignment
 
         Object.defineProperty(obj, prop, {
             get() {
@@ -17,12 +22,12 @@ export function watch<T extends object>(obj: T, onChange: () => void) {
             set(newVal) {
                 val = newVal;
                 if (isWatchable(val)) {
-//                    watch(val, onChange);
+                    watch(val, onChange);
                 }
                 onChange();
             },
-            configurable: original?.configurable,
-            enumerable: original?.enumerable,
+            configurable: original!.configurable,
+            enumerable: original!.enumerable,
         });
     }
 }
